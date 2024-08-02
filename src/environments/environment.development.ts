a223caf27b4d2d14482d5680399454399f8cfab8
Fernando Herrera <fernando.herrera85@gmail.com>
2024-08-02 15:47:47 -0400

    chore: Update GitHub API token in development environment configuration

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   baseUrl: `https://api.github.com/repos/angular/angular`,
-  gitHubToken: `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`,
+
+  // https://github.com/settings/tokens?type=beta
+  gitHubToken: ``,
 };
