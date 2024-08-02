7fd1ed22b8ea8dd00f32f3b74a37c4b36106ce2b
Fernando Herrera <fernando.herrera85@gmail.com>
2024-08-02 12:31:28 -0400

    chore: Update npm dependencies for Angular Query Experimental

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AngularQueryDevtools } from '@tanstack/angular-query-devtools-experimental';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AngularQueryDevtools],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'github-issues';
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,16 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {
+  provideAngularQuery,
+  QueryClient,
+} from '@tanstack/angular-query-experimental';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     // provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideExperimentalZonelessChangeDetection(),
+    provideAngularQuery(new QueryClient()),
   ],
 };
